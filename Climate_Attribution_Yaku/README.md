8430a2f5458986cdc4c5499b5dc1273dc1f21b8a
mjharris95 <malljes13@gmail.com>
2026-02-17 16:28:17 -0600

    Add files via upload

---
+++ Climate_Attribution_Yaku/README.md
@@ -0,0 +1,9 @@
+Steps to perform climate attribution analysis:
+
+1. Download monthly precipitation data into the directory /input_data/CMIP6/. Climate model data from the CMIP6 database can be accessed via the Earth System Grid Federation website at https://aims2.llnl.gov/search. See /input_data/CMIP6/README.md for details.
+
+2. Run the jupyter notebook /scripts/1_climate_attribution.ipynb. This script saves figure files to the /figures/ and directory.
+
+3. When running /scripts/1_climate_attribution.ipynb, certain steps will require you to run the .sh files contained in the /scripts/ directory from the command line.
+
+Please email Jared Trok (trok@stanford.edu) if you have any questions regarding this analysis code.
---
+++ Climate_Attribution_Yaku/scripts/regrid_ERA5_to_each_GCM.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+######################################################################################################
+## Regrid the ERA5 march monthly temperature data to match each climate model grid in this analysis
+## - this script uses Climate Data Operators remapbil function (Schulzweida, Uwe. 2023) 
+## - Schulzweida, Uwe. (2023). CDO User Guide (2.3.0). Zenodo. https://doi.org/10.5281/zenodo.10020800
+######################################################################################################
+
+path_to_CMIP6_data='' # add path to directory containing CMIP6 data
+
+f_grid=$path_to_CMIP6_data'/tas_Amon_ACCESS-ESM1-5_historical_r10i1p1f1_gn_185001-201412.nc'
+f_ERA5='../input_data/ERA5/march_monthly_temp_northwest_peru.nc'
+f_out=${f_ERA5/.nc/_ACCESS-ESM1-5.nc}
+echo $f_out
+cdo remapbil,$f_grid $f_ERA5 $f_out
+
+f_grid=$path_to_CMIP6_data'/tas_Amon_CanESM5_historical_r10i1p2f1_gn_185001-201412.nc'
+f_ERA5='../input_data/ERA5/march_monthly_temp_northwest_peru.nc'
+f_out=${f_ERA5/.nc/_CanESM5.nc}
+echo $f_out
+cdo remapbil,$f_grid $f_ERA5 $f_out
+
+f_grid=$path_to_CMIP6_data'tas_Amon_CNRM-CM6-1_historical_r10i1p1f2_gr_185001-201412.nc'
+f_ERA5='../input_data/ERA5/march_monthly_temp_northwest_peru.nc'
+f_out=${f_ERA5/.nc/_CNRM-CM6-1.nc}
+echo $f_out
+cdo remapbil,$f_grid $f_ERA5 $f_out
+
+f_grid=$path_to_CMIP6_data'tas_Amon_IPSL-CM6A-LR_historical_r10i1p1f1_gr_185001-201412.nc'
+f_ERA5='../input_data/ERA5/march_monthly_temp_northwest_peru.nc'
+f_out=${f_ERA5/.nc/_IPSL-CM6A-LR.nc}
+echo $f_out
+cdo remapbil,$f_grid $f_ERA5 $f_out
+
+f_grid=$path_to_CMIP6_data'tas_Amon_MIROC6_historical_r10i1p1f1_gn_185001-201412.nc'
+f_ERA5='../input_data/ERA5/march_monthly_temp_northwest_peru.nc'
+f_out=${f_ERA5/.nc/_MIROC6.nc}
+echo $f_out
+cdo remapbil,$f_grid $f_ERA5 $f_out
+
+f_grid=$path_to_CMIP6_data'tas_Amon_MIROC-ES2L_historical_r10i1p1f2_gn_185001-201412.nc'
+f_ERA5='../input_data/ERA5/march_monthly_temp_northwest_peru.nc'
+f_out=${f_ERA5/.nc/_MIROC-ES2L.nc}
+echo $f_out
+cdo remapbil,$f_grid $f_ERA5 $f_out
+
+f_grid=$path_to_CMIP6_data'tas_Amon_NorCPM1_historical_r10i1p1f1_gn_185001-201412.nc'
+f_ERA5='../input_data/ERA5/march_monthly_temp_northwest_peru.nc'
+f_out=${f_ERA5/.nc/_NorCPM1.nc}
+echo $f_out
+cdo remapbil,$f_grid $f_ERA5 $f_out
